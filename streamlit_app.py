5e4fbaf55b7aef686dd1f25cb65cdcbf9cde8eb9
slavananda <slavananda@gmail.com>
2024-07-29 21:57:32 +0200

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,53 +1,17 @@
-import os
-import openai
 import streamlit as st
 from parse_hh import get_candidate_info, get_job_description
 
-# Ensure the OpenAI API key is set
-api_key = os.getenv("OPENAI_API_KEY")
-st.write(f"OpenAI API Key: {api_key}")
-
-# Initialize OpenAI client
-client = openai.Client(api_key=api_key)
-
-SYSTEM_PROMPT = """
-Проскорь кандидата, насколько он подходит для данной вакансии.
-
-Сначала напиши короткий анализ, который будет пояснять оценку.
-Отдельно оцени качество заполнения резюме (понятно ли, с какими задачами сталкивался кандидат и каким образом их решал?). Эта оценка должна учитываться при выставлении финальной оценки - нам важно нанимать таких кандидатов, которые могут рассказать про свою работу
-Потом представь результат в виде оценки от 1 до 10.
-""".strip()
-
-def request_gpt(system_prompt, user_prompt):
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": user_prompt},
-        ],
-        max_tokens=1000,
-        temperature=0,
-    )
-    return response.choices[0].message.content
-
 st.title("CV Scoring App")
 
-job_description_url = st.text_area("Enter the job description url")
-cv_url = st.text_area("Enter the CV url")
+job_description_url = st.text_input("Введите URL описания вакансии:")
+if job_description_url:
+    job_description = get_job_description(job_description_url)
+    st.markdown(job_description)
 
-if st.button("Score CV"):
-    with st.spinner("Scoring CV..."):
-        job_description = get_job_description(job_description_url)
-        cv = get_candidate_info(cv_url)
+candidate_info_url = st.text_input("Введите URL резюме кандидата:")
+if candidate_info_url:
+    candidate_info = get_candidate_info(candidate_info_url)
+    st.markdown(candidate_info)
 
-        st.write("Job description:")
-        st.write(job_description)
-        st.write("CV:")
-        st.write(cv)
-
-        user_prompt = f"# ВАКАНСИЯ\n{job_description}\n\n# РЕЗЮМЕ\n{cv}"
-        response = request_gpt(SYSTEM_PROMPT, user_prompt)
-
-    st.write(response)
 
 
